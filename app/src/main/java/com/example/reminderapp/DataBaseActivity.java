2f14a0f11290fded50ec22eb0ca36766bd09b689
Sadee <180204066@aust.edu>
2021-02-06 22:22:34 +0600

    Splash Screen

---
+++ app/src/main/java/com/example/reminderapp/DataBaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class DataBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_base);
+    }
+}
