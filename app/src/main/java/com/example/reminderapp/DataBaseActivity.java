7ec595fb0520e15586ed407c9f61bc3c85e6245f
Sadee <180204066@aust.edu>
2021-02-07 23:09:45 +0600

    Login_page

--- app/src/main/java/com/example/reminderapp/DataBaseActivity.java
+++ app/src/main/java/com/example/reminderapp/DataBaseActivity.java
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DataBaseActivity extends AppCompatActivity {
+    private EditText user_name, password_view;
+    private TextView  reg_button;
+    private Button button_login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user_name = (EditText)findViewById(R.id.user_name);
+
+        password_view = (EditText)findViewById(R.id.password_view);
+
+        button_login = (Button) findViewById(R.id.button_login);
+        reg_button =(TextView) findViewById(R.id.reg_button);
+
         setContentView(R.layout.activity_data_base);
+    }
+
+    public void Login(View view) {
+
+
     }
 }
