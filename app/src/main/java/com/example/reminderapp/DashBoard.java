d72c5cd45d49f5be98bd1d4e217b386fcbb8154d
noman27 <blt.normanbruce@yahoo.com>
2021-02-08 11:06:12 +0600

    Login and LogOut done.User interface design needed

--- app/src/main/java/com/example/reminderapp/DashBoard.java
+++ app/src/main/java/com/example/reminderapp/DashBoard.java
@@ -2,13 +2,26 @@ package com.example.reminderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashBoard extends AppCompatActivity {
-
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        logout=(Button)findViewById(R.id.log_out);
+    }
+
+    public void LogOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        startActivity(new Intent(DashBoard.this,DataBaseActivity.class));
     }
 }
--- app/src/main/java/com/example/reminderapp/DataBaseActivity.java
+++ app/src/main/java/com/example/reminderapp/DataBaseActivity.java
@@ -1,15 +1,21 @@
 package com.example.reminderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
@@ -38,6 +44,55 @@ public class DataBaseActivity extends AppCompatActivity {
     }
 
     public void Login(View view) {
+        String email=user_email.getText().toString().trim();
+        String pass=user_password.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            user_email.setError("Email Required");
+            user_email.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            user_email.setError("Valid Email Address Required");
+            user_email.requestFocus();
+            return;
+        }
+
+        if(pass.isEmpty())
+        {
+            user_password.setError("Password required");
+            user_password.requestFocus();
+            return;
+        }
+
+        if(pass.length()<6)
+        {
+            user_password.setError("Minimum Password length is 6");
+            user_password.requestFocus();
+            return;
+        }
+
+        Intent intent=new Intent(this, DashBoard.class);
+        //Login function firebase
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        finish();
+                        startActivity(intent);
+                        Toast.makeText(DataBaseActivity.this,"Logged In",Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(DataBaseActivity.this,"Failed to login. Please check your credentials",Toast.LENGTH_LONG).show();
+                    }
+            }
+        });
+
     }
 
     public void CreateAcc(View view) {
