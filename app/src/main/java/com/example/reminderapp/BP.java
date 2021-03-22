73f1cfe15f0b5035b1319eab0c82b17f799a53ed
Sadee <180204066@aust.edu>
2021-03-22 14:42:16 +0600

    Dashboard,log_in,Design

--- app/src/main/java/com/example/reminderapp/BP.java
+++ app/src/main/java/com/example/reminderapp/BP.java
@@ -1,14 +1,33 @@
 package com.example.reminderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class BP extends AppCompatActivity {
-
+    FirebaseUser user;
+    DatabaseReference databaseReference;
+    String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_p);
+
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        uid=user.getUid();
+
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+
+    }
+
+    public void goDocList(View view) {
     }
 }
