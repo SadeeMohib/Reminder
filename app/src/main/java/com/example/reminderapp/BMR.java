49978c076cbedbd225ddba1a407c34fb604f0808
noman27 <blt.normanbruce@yahoo.com>
2021-04-02 12:51:17 +0600

    Project shesh kor vai plzz

--- app/src/main/java/com/example/reminderapp/BMR.java
+++ app/src/main/java/com/example/reminderapp/BMR.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class BMR extends AppCompatActivity {
@@ -32,9 +33,11 @@ public class BMR extends AppCompatActivity {
 
         user= FirebaseAuth.getInstance().getCurrentUser();
         uid=user.getUid();
+        databaseReference= FirebaseDatabase.getInstance().getReference();
 
         bmr=(TextView)findViewById(R.id.bmr);
 
+        getData(databaseReference,uid);
 
         PhotoView photoView = (PhotoView) findViewById(R.id.imageView5);
         photoView.setImageResource(R.drawable.bmr2);
